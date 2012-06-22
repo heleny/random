fb45cf601b4f5ccb5970e09b9cbb78915c01b86f
Helen <helen@Kingdrass-iMac.local>
2012-06-22 15:30:17 -0400

    add static table app

---
+++ StaticTable/StaticTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StaticTable
+//
+//  Created by Helen on 12-06-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StaticTable/StaticTable/StaticTableViewControllerViewController.h
@@ -0,0 +1,17 @@
+//
+//  StaticTableViewControllerViewController.h
+//  StaticTable
+//
+//  Created by Helen on 12-06-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StaticTableViewControllerViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *makeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *modelLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
