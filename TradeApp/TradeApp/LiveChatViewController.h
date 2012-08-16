136f7e514cdd3d29dfcd45a211ae1b60de399e4f
Helen <helen@Helens-iMac.local>
2012-08-16 16:13:36 -0400

    added Bubble Live Chat

---
+++ TradeApp/TradeApp/LiveChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  LiveChatViewController.h
+//  TradeApp
+//
+//  Created by Helen Yang on 12-08-16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiveChatViewController : UIViewController
+
+@end
---
+++ TradeApp/TradeApp/LiveChatViewController.m
@@ -0,0 +1,43 @@
+//
+//  LiveChatViewController.m
+//  TradeApp
+//
+//  Created by Helen Yang on 12-08-16.
+//
+//
+
+#import "LiveChatViewController.h"
+
+@interface LiveChatViewController ()
+
+@end
+
+@implementation LiveChatViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
